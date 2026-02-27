d82193fb9cf73e3c4610584f8c7078cb5ff46952
BlackChihiro <blackayano00@gmail.com>
2026-02-27 19:17:59 +0900

    refactor: 프로젝트 구조 변경

--- lib/core/widgets/appbar/MainAppBar.dart
+++ lib/core/widgets/appbar/MainAppBar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class MainAppBar extends ConsumerStatefulWidget {
   const MainAppBar({super.key});
@@ -12,20 +13,26 @@ class _MainAppBarState extends ConsumerState<MainAppBar> {
   @override
   Widget build(BuildContext build) {
     return SizedBox(
-      width: 140,
+      width: 155,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          ClipOval(
-            child: Image.asset(
-              fit: BoxFit.cover,
-              "assets/Icons/main_icon.jpg",
-              width: 45,
-              height: 45,
+          GestureDetector(
+            onTap: () {
+              build.go('/');
+            },
+            child: ClipOval(
+              child: Image.asset(
+                "assets/icons/main_icon.jpg",
+                width: 40,
+                height: 40,
+                fit: BoxFit.cover,
+                filterQuality: FilterQuality.high,
+              ),
             ),
           ),
           Spacer(),
-          Text("별자리 카페")
+          Text("섀버 별자리 Cafe")
         ],
       ),
     );
