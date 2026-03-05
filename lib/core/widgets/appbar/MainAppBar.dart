983dbb7eeeca26613fa2d59524694b0bd8ef5cb7
BlackChihiro <blackayano00@gmail.com>
2026-03-05 13:39:35 +0900

    feat: 모바일 환경에서 메뉴바(drawer) 사용

--- lib/core/widgets/appbar/MainAppBar.dart
+++ lib/core/widgets/appbar/MainAppBar.dart
@@ -3,7 +3,14 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 class MainAppBar extends ConsumerStatefulWidget {
-  const MainAppBar({super.key});
+  final bool showMenuButton;
+  final VoidCallback? onMenuPressed;
+
+  const MainAppBar({
+    super.key,
+    this.showMenuButton = false,
+    this.onMenuPressed,
+  });
 
   @override
   ConsumerState<MainAppBar> createState() => _MainAppBarState();
@@ -11,28 +18,33 @@ class MainAppBar extends ConsumerStatefulWidget {
 
 class _MainAppBarState extends ConsumerState<MainAppBar> {
   @override
-  Widget build(BuildContext build) {
+  Widget build(BuildContext context) {
     return SizedBox(
       width: 155,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          GestureDetector(
-            onTap: () {
-              build.go('/');
-            },
-            child: ClipOval(
-              child: Image.asset(
-                "assets/icons/main_icon.jpg",
-                width: 40,
-                height: 40,
-                fit: BoxFit.cover,
-                filterQuality: FilterQuality.high,
+          if (widget.showMenuButton)
+            IconButton(
+              onPressed: widget.onMenuPressed,
+              icon: const Icon(Icons.menu, size: 30),
+              tooltip: 'Menu',
+            )
+          else
+            GestureDetector(
+              onTap: () => context.go('/'),
+              child: ClipOval(
+                child: Image.asset(
+                  "assets/icons/main_icon.jpg",
+                  width: 40,
+                  height: 40,
+                  fit: BoxFit.cover,
+                  filterQuality: FilterQuality.high,
+                ),
               ),
             ),
-          ),
-          Spacer(),
-          Text("섀버 별자리 Cafe")
+          const Spacer(),
+          const Text("섀버 별자리 Cafe"),
         ],
       ),
     );
