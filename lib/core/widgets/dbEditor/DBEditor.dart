a530b272b7ef91c2652ef2067d057c21c2d942bd
BlackChihiro <blackayano00@gmail.com>
2026-04-11 14:37:19 +0900

    chore: readonly DBEditor에선 Usage 사용 X

--- lib/core/widgets/dbEditor/DBEditor.dart
+++ lib/core/widgets/dbEditor/DBEditor.dart
@@ -35,67 +35,75 @@ class _DBEditorState extends State<DBEditor> {
   final GlobalKey editKey = GlobalKey();
   final GlobalKey saveKey = GlobalKey();
 
-  Widget createDBEditor(DBController controller) {
-    return Usage(
-        usageKey: EditorUsage.key,
-        steps: EditorUsage.steps(
-          columnKey: columnKey,
-          viewKey: viewKey,
-          addKey: addKey,
-          deleteKey: deleteKey,
-          editKey: editKey,
-          saveKey: saveKey,
-        ),
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            final maxH = constraints.maxHeight.isFinite ? constraints.maxHeight : 500.0;
-            final editorH = maxH < 500 ? maxH : 500.0;
+  Widget buildDBEditor(DBController controller) {
+    if (widget.readonly) {
+      return DBEditor(controller);
+    } else {
+      return Usage(
+          usageKey: EditorUsage.key,
+          steps: EditorUsage.steps(
+            columnKey: columnKey,
+            viewKey: viewKey,
+            addKey: addKey,
+            deleteKey: deleteKey,
+            editKey: editKey,
+            saveKey: saveKey,
+          ),
+          child: DBEditor(controller)
+      );
+    }
+  }
 
-            return SizedBox(
-              width: 500,
-              height: editorH,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  // border: Border.all(color: Colors.black, width: 2),
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Color(0xFF000D27).withOpacity(0.12),  // rgba(0, 13, 39, 0.12)
-                      blurRadius: 24,                              // 24px 흐림
-                      offset: Offset(0, 8),                        // 0px x, 8px y
-                    ),
-                  ],
+  Widget DBEditor(DBController controller) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final maxH = constraints.maxHeight.isFinite ? constraints.maxHeight : 500.0;
+        final editorH = maxH < 500 ? maxH : 500.0;
+
+        return SizedBox(
+          width: 500,
+          height: editorH,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              // border: Border.all(color: Colors.black, width: 2),
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0xFF000D27).withOpacity(0.12),  // rgba(0, 13, 39, 0.12)
+                  blurRadius: 24,                              // 24px 흐림
+                  offset: Offset(0, 8),                        // 0px x, 8px y
                 ),
-                padding: const EdgeInsets.all(ConstSize.mediumWidth),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    DBColumns(
-                        key: columnKey,
-                        controller: controller
-                    ),
-                    SizedBox(height: ConstSize.mediumHeight),
-                    Expanded(child: DBDataView(
-                        key: viewKey,
-                        controller: controller
-                    )),
-                    if (widget.readonly==false)...[
-                      SizedBox(height: ConstSize.mediumHeight),
-                      EditorBar(
-                          addKey: addKey,
-                          deleteKey: deleteKey,
-                          editKey: editKey,
-                          saveKey: saveKey,
-                          controller: controller
-                      ),
-                    ]
-                  ],
+              ],
+            ),
+            padding: const EdgeInsets.all(ConstSize.mediumWidth),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                DBColumns(
+                    key: columnKey,
+                    controller: controller
                 ),
-              ),
-            );
-          },
-        )
+                SizedBox(height: ConstSize.mediumHeight),
+                Expanded(child: DBDataView(
+                    key: viewKey,
+                    controller: controller
+                )),
+                if (widget.readonly==false)...[
+                  SizedBox(height: ConstSize.mediumHeight),
+                  EditorBar(
+                      addKey: addKey,
+                      deleteKey: deleteKey,
+                      editKey: editKey,
+                      saveKey: saveKey,
+                      controller: controller
+                  ),
+                ]
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 
@@ -114,7 +122,7 @@ class _DBEditorState extends State<DBEditor> {
               snapshot.data!,
               repository: widget.repository
           );
-          return createDBEditor(_controller!);
+          return buildDBEditor(_controller!);
         } else {
           return const Text("데이터 없음");
         }
