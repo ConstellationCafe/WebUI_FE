3cd4dec1e8feac47edc8156e10014cf666182693
BlackChihiro <blackayano00@gmail.com>
2026-08-13 17:34:40 +0900

    refactor: 카테고리별 라우터 분리

---
+++ lib/feature/contents/academy/routes/academy_routes.dart
@@ -0,0 +1,12 @@
+import 'package:go_router/go_router.dart';
+import '../../../../router/no_aim_page.dart';
+import '../class_content_record/page/class_content_record.dart';
+
+
+final academyRoutes = <GoRoute>[
+  GoRoute(
+    path: '/academy/lesson_record',
+    pageBuilder: (context, state) =>
+        noAnim(state, const LessonRecordPage()),
+  )
+];
