ecf97fc1954848058b544aad9ec1c77040664a12
BlackChihiro <blackayano00@gmail.com>
2026-08-13 22:16:42 +0900

    fix: 불필요한 로그인 확인 제거

--- lib/feature/home/home_page/pages/home_contents.dart
+++ lib/feature/home/home_page/pages/home_contents.dart
@@ -3,22 +3,16 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:constellation_cafe/feature/auth/notifier/login_check_notifier.dart';
 
+import '../../../../core/constants/const_size.dart';
+
 class HomeContent extends ConsumerWidget {
   const HomeContent({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final login = ref.watch(loginCheckProvider);
-    return login.when(
-      loading: () => const SizedBox.shrink(),
-      error: (_, __) => const SizedBox.shrink(),
-      data: (isLoggedIn) {
-        if (!isLoggedIn) return const SizedBox.shrink();
-        return Text(
-          "홈페이지 메인 컨텐츠",
-          style: TextStyle(fontSize: 24),
-        );
-      },
+    return Text(
+      "홈페이지 메인 컨텐츠",
+      style: TextStyle(fontSize: ConstSize.largeTextSize),
     );
   }
 }
