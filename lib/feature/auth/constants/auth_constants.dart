82c141a23e3ebf9819aceabf8c8e13f991974485
BlackChihiro <blackayano00@gmail.com>
2026-08-13 17:35:15 +0900

    refactor: feature별 constants 분리

---
+++ lib/feature/auth/constants/auth_constants.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AuthConstants {
+  // Login Card
+  static const double loginCardMaxWidth = 360.0;
+  static const double loginCardHorizontalPadding = 36.0;
+  static const double loginCardVerticalPadding = 28.0;
+  static const double loginCardRadius = 18.0;
+
+  // Login Header
+  static const double loginLogoSize = 52.0;
+  static const double loginLogoRadius = 10.0;
+  static const double loginTitleFontSize = 26.0;
+
+  // Login Shadow
+  static const Color loginShadowColor = Color(0x14000000);
+  static const double loginShadowBlurRadius = 20.0;
+  static const double loginShadowOffsetY = 6.0;
+
+  // Discord Login Button
+  static const double discordLoginButtonDesktopWidth = 160.0;
+  static const double discordLoginButtonHeight = 40.0;
+  static const double discordLoginButtonRadius = 5.0;
+  static const double discordLoginButtonIconSize = 18.0;
+  static const Color discordLoginButtonColor = Colors.blueAccent;
+}
