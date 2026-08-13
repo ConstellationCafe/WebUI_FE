e1a718d5fe6d024c05f9d2d9be41528e5941e64b
BlackChihiro <blackayano00@gmail.com>
2026-08-13 17:35:55 +0900

    refactor: InputMembershipData에서 저장 버튼 분리

---
+++ lib/feature/profile/widgets/save_membership_button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import '../constants/profile_constants.dart';
+
+class SaveMembershipButton extends StatelessWidget {
+  final bool isLoading;
+  final VoidCallback? onPressed;
+
+  const SaveMembershipButton({
+    super.key,
+    required this.isLoading,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return ElevatedButton(
+      onPressed: isLoading ? null : onPressed,
+      child: isLoading
+          ? SizedBox(
+              width: ProfileConstants.loadingIndicatorSize,
+              height: ProfileConstants.loadingIndicatorSize,
+              child: CircularProgressIndicator(
+                strokeWidth:
+                ProfileConstants.loadingIndicatorStrokeWidth,
+                color: theme.colorScheme.onPrimary,
+              ),
+            )
+          : Text(
+              '저장',
+              style: theme.textTheme.labelMedium?.copyWith(
+                color: theme.colorScheme.onPrimary,
+              ),
+            ),
+    );
+  }
+}
