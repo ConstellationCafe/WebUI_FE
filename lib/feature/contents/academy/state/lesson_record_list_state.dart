956bbc7c51a8b9f0969d21ff4efb7826c80c116b
BlackChihiro <blackayano00@gmail.com>
2026-08-19 17:46:43 +0900

    refactor: LessonRecordSummary를 ResponseDto로 변경

--- lib/feature/contents/academy/state/lesson_record_list_state.dart
+++ lib/feature/contents/academy/state/lesson_record_list_state.dart
@@ -2,7 +2,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 import '../domain/model/academy.dart';
 import '../domain/model/academy_class.dart';
-import '../domain/model/lesson_record_summary.dart';
+import '../domain/dto/response/lesson_record_response.dart';
 import '../domain/model/subject.dart';
 
 part 'lesson_record_list_state.freezed.dart';
@@ -18,7 +18,7 @@ abstract class LessonRecordListState
     bool isFilterLoading,
 
     @Default([])
-    List<LessonRecordSummary> records,
+    List<LessonRecordResponse> records,
 
     @Default([])
     List<Academy> academies,
