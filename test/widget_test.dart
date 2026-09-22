3ac8b1a49f40b6779ef59aec218a6cb93663f46c
BlackChihiro <39819769+BlackChihiro@users.noreply.github.com>
2026-09-22 11:34:06 +0900

    style: normalize widget test formatting

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,15 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('renders a Flutter widget on the web platform', (
-    WidgetTester tester,
-  ) async {
+  testWidgets('renders a Flutter widget on the web platform', (tester) async {
     await tester.pumpWidget(
-      const MaterialApp(
-        home: Scaffold(
-          body: Text('WebUI FE'),
-        ),
-      ),
+      const MaterialApp(home: Text('WebUI FE')),
     );
 
     expect(find.text('WebUI FE'), findsOneWidget);
