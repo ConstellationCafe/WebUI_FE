9b77d136160c8ce737688cab646567774f632f1b
BlackChihiro <39819769+BlackChihiro@users.noreply.github.com>
2026-09-22 11:34:19 +0900

    style: use formatter-stable widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,9 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('renders a Flutter widget on the web platform', (tester) async {
-    await tester.pumpWidget(
-      const MaterialApp(home: Text('WebUI FE')),
-    );
+    await tester.pumpWidget(const MaterialApp(home: Text('WebUI FE')));
 
     expect(find.text('WebUI FE'), findsOneWidget);
   });
