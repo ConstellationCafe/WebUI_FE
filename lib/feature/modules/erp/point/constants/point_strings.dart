2bd8ca8c9262c12331df54701d3743f827bc7d10
BlackChihiro <39819769+BlackChihiro@users.noreply.github.com>
2026-09-23 16:48:05 +0900

    style: 포인트 관리 코드 포맷 정리

--- lib/feature/modules/erp/point/constants/point_strings.dart
+++ lib/feature/modules/erp/point/constants/point_strings.dart
@@ -3,8 +3,7 @@ class PointStrings {
   static const searchHint = 'Discord ID 검색';
   static const members = '재적 회원';
   static const noMembers = '조회된 재적 회원이 없습니다.';
-  static const selectMember =
-      '회원을 선택하면 포인트 내역을 확인할 수 있습니다.';
+  static const selectMember = '회원을 선택하면 포인트 내역을 확인할 수 있습니다.';
   static const balance = '보유 포인트';
   static const history = '포인트 내역';
   static const deposit = '입금';
